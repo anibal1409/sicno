6c038f94bc3b172195acfcc57a3d8594e8182d4b
Anibal Farinas <anibal.antonio1409@gmail.com>
2020-09-05 18:53:34 -0400

    base componente admin

--- src/app/core/admin/admin.module.ts
+++ src/app/core/admin/admin.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -10,6 +13,10 @@ import { AdminComponent } from './admin.component';
   imports: [
     CommonModule,
     AdminRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class AdminModule { }
